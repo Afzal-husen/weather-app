21e4d74db48fefbbe1b690cba9f349fe5d85c02d
Afzal Diwan <afzalhusen3@gmail.com>
2023-04-05 21:00:08 +0530

    initial commit

---
+++ src/fetchAPI.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': process.env.REACT_APP_GEO_API_KEY,
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = 'http://api.openweathermap.org/data/2.5';
+
